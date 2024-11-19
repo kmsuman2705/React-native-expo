99055faff4249c3ef53318c7a405815bcf41652c
kmsuman27 <kmsuman27@gmail.com>
2024-11-19 16:55:43 +0530

    tab bar final update

--- app/(tab)/_layout.tsx
+++ app/(tab)/_layout.tsx
@@ -1,13 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { AntDesign, FontAwesome } from '@expo/vector-icons';
 
 const TabRoot = () => {
   return (
     <Tabs>
-        <Tabs.Screen name="index" options={{title: "Home"}}/>
-        <Tabs.Screen name="about" options={{title:"About Us"}}/>
-        <Tabs.Screen name="profile" options={{title:"Profile"}}/>
+        <Tabs.Screen name="index" options={{title: "Home",           tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+}}/>
+        <Tabs.Screen name="about" options={{title:"About Us",           tabBarIcon: ({ color }) => <AntDesign size={28} name
+="contacts
+" color={color} />,}}/>
+        <Tabs.Screen name="profile" options={{title:"Profile",          tabBarIcon: ({ color }) => (<FontAwesome size={28} name="user-o" color={color} />),
+}}/>
     </Tabs>
   );
 
